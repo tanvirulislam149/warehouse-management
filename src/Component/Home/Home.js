132b023b808e22dfe08a114778f2d61c6ec1ce0b
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-05-05 22:24:23 +0600

    services done

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Services from '../Services/Services';
+import Specialities from '../Specialities/Specialities';
 import TopItems from '../TopItems/TopItems';
 import Banner from './Banner/Banner';
 
@@ -7,6 +9,8 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <TopItems></TopItems>
+            <Specialities></Specialities>
+            <Services></Services>
         </div>
     );
 };
