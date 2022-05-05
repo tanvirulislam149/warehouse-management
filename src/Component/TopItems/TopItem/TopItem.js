e5b5cdbd82bc54c4ac52c6dbee0051f0f2af2c4f
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-05-05 22:33:24 +0600

    top items fully done

--- src/Component/TopItems/TopItem/TopItem.js
+++ src/Component/TopItems/TopItem/TopItem.js
@@ -13,6 +13,9 @@ const TopItem = ({ product }) => {
                     <Card.Body>
                         <Card.Title className='title fw-bold'>{product.name}</Card.Title>
                         <Card.Text className='text-justify'>{product.shortDescription}</Card.Text>
+                        <h6>Price: ${product.price}</h6>
+                        <h6>Quantity: ${product.quantity}</h6>
+                        <h6>Supplier: ${product.supplier}</h6>
                         <button className='update-btn'>Update</button>
                     </Card.Body>
                 </div>
