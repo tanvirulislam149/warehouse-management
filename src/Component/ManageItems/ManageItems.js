9fe8968d292fb1961d830dfb2c6411c5cc3289e8
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-05-07 03:33:15 +0600

    delete done

--- src/Component/ManageItems/ManageItems.js
+++ src/Component/ManageItems/ManageItems.js
@@ -1,4 +1,7 @@
+import { faArrowRight, faRemoveFormat, faRemo, faTrash, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
 
 const ManageItems = () => {
     const [products, setProducts] = useState([]);
@@ -8,6 +11,19 @@ const ManageItems = () => {
             .then(res => res.json())
             .then(data => setProducts(data));
     }, [])
+
+    const deleteItem = (id) => {
+        fetch(`http://localhost:5000/product/${id}`, {
+            method: "DELETE",
+        })
+            .then(res => res.json())
+            .then(data => {
+                const newProducts = products.filter(product => product._id !== id);
+                setProducts(newProducts);
+            });
+    }
+
+
     return (
         <div className='container my-5'>
             <h1 className='text-center fw-bold text-color mt-5 item-header'>All Items</h1>
@@ -18,14 +34,16 @@ const ManageItems = () => {
                         products.map(product =>
                             <tr className='border-1' key={product._id}>
                                 <th className='text-center'><img style={{ width: "50px" }} src={product.picture} alt="" /></th>
-                                <th className='text-center'>{product.name}</th>
-                                <th className='text-center'>Price: ${product.price}</th>
-                                <th className='text-center'><button className='text-white bg-danger border-0 fw-bold px-2'>Delete</button></th>
+                                <th className='text-center px-3 py-3'>{product.name}</th>
+                                <th className='text-center px-3'>Price: ${product.price}</th>
+                                <th className='text-center fs-1 px-3'><button onClick={() => deleteItem(product._id)} className='border-0 bg-white text-danger'><FontAwesomeIcon icon={faTrashAlt} /></button></th>
                             </tr>
                         )
                     }
                 </tbody>
             </table>
+            <Button className='fw-bold ms-auto d-block mt-5' variant='primary'>Add New Item <FontAwesomeIcon icon={faArrowRight} />
+            </Button>
         </div>
     );
 };
--- src/Component/TopItems/TopItems.js
+++ src/Component/TopItems/TopItems.js
@@ -1,24 +1,26 @@
 import React, { useEffect, useState } from 'react';
-import { Row } from 'react-bootstrap';
+import { Button, Row } from 'react-bootstrap';
 import TopItem from './TopItem/TopItem';
 import "./TopItems.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom';
+
 
 const TopItems = () => {
     const [items, setItems] = useState([]);
 
-    useEffect(() =>{
+    useEffect(() => {
         fetch("http://localhost:5000/items")
-        .then(res => res.json())
-        .then(data => setItems(data));
-    },[])
+            .then(res => res.json())
+            .then(data => setItems(data));
+    }, [])
 
-    // const [products, setProducts] = useState([]);
+    let navigate = useNavigate();
+    const goToManageItems = () => {
+        navigate("/manageItems");
+    }
 
-    // useEffect(() => {
-    //     fetch("http://localhost:5000/items")
-    //         .then(res => res.json())
-    //         .then(data => setProducts(data));
-    // }, [])
     return (
         <div className='container my-5'>
             <h1 className='text-center fw-bold text-color mt-5 item-header'>Top Items</h1>
@@ -29,7 +31,8 @@ const TopItems = () => {
                     items.map(item => <TopItem key={item._id} item={item}></TopItem>)
                 }
             </Row>
-            
+            <Button onClick={goToManageItems} className='fw-bold ms-auto d-block mt-5' variant='primary'>Manage Items <FontAwesomeIcon icon={faArrowRight} />
+            </Button>
         </div>
     );
 };
