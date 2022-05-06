f0ba19ccdb4995c5f1da4a43ad22d223493e588f
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-05-07 01:38:38 +0600

    restock done

--- src/Component/TopItems/ItemDetail/ItemDetail.js
+++ src/Component/TopItems/ItemDetail/ItemDetail.js
@@ -19,22 +19,39 @@ const ItemDetail = () => {
     const handleQuantity = () => {
         if (quantityNumber > 0) {
             fetch(`http://localhost:5000/updateQuantity/${params.id}`, {
-                method: "PUT", 
+                method: "PUT",
                 headers: {
-                    "content-type" : "application/json",
+                    "content-type": "application/json",
                 },
-                body: JSON.stringify({quantityNumber}),
+                body: JSON.stringify({ quantityNumber }),
             })
-            .then((res) => res.json())
-            .then((json) => {
-                const quantity = quantityNumber;
-                const newQuantity = parseInt(quantity) - 1;
-                setquantityNumber(newQuantity);
-                console.log(json);
-            })
-            
+                .then((res) => res.json())
+                .then((json) => {
+                    const quantity = quantityNumber;
+                    const newQuantity = parseInt(quantity) - 1;
+                    setquantityNumber(newQuantity);
+                })
+
         }
     }
+
+    const restockSubmit = (event) => {
+        event.preventDefault();
+        let restockNumber = parseInt(event.target.restock.value);
+        fetch(`http://localhost:5000/restockQuantity/${params.id}`, {
+            method: "PUT",
+            headers: {
+                "content-type": "application/json",
+            },
+            body: JSON.stringify({ restockNumber }),
+        })
+            .then((res) => res.json())
+            .then((json) => {
+                setquantityNumber(restockNumber);
+            })
+        event.target.restock.value = "";
+    }
+
     return (
         <Card className='w-50 mx-auto border-0'>
             <Card.Img className='w-50 mx-auto' variant="top" src={item?.picture} />
@@ -47,6 +64,10 @@ const ItemDetail = () => {
                 <h6>Quantity: {quantityNumber}</h6>
                 <h6 className={quantityNumber <= 0 ? 'text-danger fw-bold d-block' : "d-none"}>Sold Out</h6>
                 <Button onClick={handleQuantity} variant="primary">Delivered</Button>
+                <form className='mt-4' onSubmit={restockSubmit}>
+                    <input type="number" name="restock" id="" /><br />
+                    <input className='border-0 bg-success text-white fw-bold mt-3' type="submit" value="Restock" />
+                </form>
             </Card.Body>
         </Card>
     );
