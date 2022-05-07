ffe7052b2d65d0161fbdcd2d7a3d508b98ff7ff9
tanvirulislam149 <tanvirulislam149@gmail.com>
2022-05-07 14:56:34 +0600

    add item done

--- src/Component/AddItem/AddItem.js
+++ src/Component/AddItem/AddItem.js
@@ -6,8 +6,19 @@ import "./AddItem.css";
 const AddItem = () => {
     const { register, handleSubmit, reset } = useForm();
     const onSubmit = data => {
-        console.log(data);
-        reset();
+        fetch(`http://localhost:5000/insertItem`, {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "content-type": "application/json",
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                reset();
+            });
+
     };
 
 
@@ -16,14 +27,14 @@ const AddItem = () => {
             <h1 className='text-center fw-bold text-color mt-5 item-header'>Add New Item</h1>
             <hr className='mb-2 mx-auto' style={{ height: "5px", color: "#002266", width: "100px", borderRadius: "5px" }} />
             <form className='text-center' onSubmit={handleSubmit(onSubmit)}>
-                <input className='w-25 mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Your Name" {...register("userName", { required: true })} /> <br />
-                <input className='w-25 mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Item Name" {...register("itemName", { required: true })} /><br />
-                <input className='w-25 mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Short Description" {...register("shortDescription", { required: true })} /><br />
-                <input className='w-25 mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Description" {...register("description", { required: true })} /><br />
-                <input className='w-25 mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Supplier Name" {...register("supplierName", { required: true })} /><br />
-                <input className='w-25 mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Image Url" {...register("imageUrl", { required: true })} /><br />
-                <input className='w-25 mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} type="number" placeholder='Enter Price' {...register("price")} /><br />
-                <input className='w-25 mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Item Quantity" type="number" {...register("quantity")} /><br />
+                <input className='input-field mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Your Name" {...register("userName", { required: true })} /> <br />
+                <input className='input-field mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Item Name" {...register("name", { required: true })} /><br />
+                <input className='input-field mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Short Description" {...register("shortDescription", { required: true })} /><br />
+                <input className='input-field mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Description" {...register("description", { required: true })} /><br />
+                <input className='input-field mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Supplier Name" {...register("supplierName", { required: true })} /><br />
+                <input className='input-field mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Image Url" {...register("picture", { required: true })} /><br />
+                <input className='input-field mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} type="number" placeholder='Enter Price' {...register("price")} /><br />
+                <input className='input-field mx-auto my-1 border-0 py-1 px-2' style={{ backgroundColor: "#ecf2f9", borderRadius: "10px" }} placeholder="Enter Item Quantity" type="number" {...register("quantity")} /><br />
                 <input className='update-btn' type="submit" />
             </form>
         </div>
